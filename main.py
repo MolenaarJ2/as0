690ba9823a25ecba52c0ac4a235b207ea3fbd3f6
Jelle <molenaar.jd2@gmail.com>
2025-09-08 17:16:38 +0200

    Complete project setup and add numpy script

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ def main() -> None:
 
 
 def add_two_integer_lists(a: list[int], b: list[str]) -> int:
-    arr = np.array(a)+np.array(b)
+    """Adding 2 integer lists"""
+    arr = np.array(a) + np.array(b)
     print(f"My added array: {arr}")
+    return arr
 
 
 if __name__ == "__main__":
-    add_two_integer_lists([1, 2, 3], [4, 5, 6])
+    arr = add_two_integer_lists([1, 2, 3], [4, 5, 6])
